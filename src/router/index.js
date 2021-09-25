469d3a9ac2cf6e25502ebbd17a8f0c2820a9c542
lightcolor <13564722173@163.com>
2021-09-26 01:22:01 +0800

    '创建tabbar'

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,43 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+
+const Home = () =>
+    import ('@/views/home/Home.vue')
+const Category = () =>
+    import ('@/views/category/Category.vue')
+const Profile = () =>
+    import ('@/views/profile/Profile.vue')
+const Shopcart = () =>
+    import ('@/views/shopcart/Shopcart.vue')
+
+const routes = [{
+        path: '',
+        redirect: '/home'
+    },
+    {
+        path: '/home',
+        component: Home
+    },
+    {
+        path: '/category',
+        component: Category
+    },
+    {
+        path: '/profile',
+        component: Profile
+    },
+    {
+        path: '/cart',
+        component: Shopcart
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes,
+    mode: 'history'
 })
 
 export default router
---
+++ src/views/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+<h1>我是主页组件</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
